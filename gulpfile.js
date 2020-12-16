371eca11161e30c129454d5778c1c31769883110
navneet <navneet3265@gmail.com>
2020-12-16 10:53:46 -0600

    add a clean Task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const {src, dest, parallel, series} = require('gulp');
+const del = require('del');
+
+function cleanTask() {
+  return del('dist/');
+}
 
 function htmlTask() {
   return src('src/*.html')
@@ -15,7 +20,8 @@ function scriptTask() {
     .pipe(dest('dist/scripts/'));
 }
 
+exports.clean = cleanTask;
 exports.html = htmlTask;
 exports.styles = stylesTask;
 exports.scripts = scriptTask;
-exports.default = series( parallel(htmlTask, stylesTask, scriptTask))
+exports.default = series( cleanTask, parallel(htmlTask, stylesTask, scriptTask));
