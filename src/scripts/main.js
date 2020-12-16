47ee71b26399590f4951f3aea3f4a9ebac09c6cd
navneet <navneet3265@gmail.com>
2020-12-16 16:55:25 -0600

    get movies from api and add all the functionality in the js file

---
+++ src/scripts/main.js
@@ -0,0 +1,44 @@
+const moviesListElement = document.querySelector(".movies");
+const searchbar = document.querySelector("form");
+
+function getMovie(value  , attribute = 's') {
+  return fetch(
+    `http://omdbapi.com/?apikey=89e9df8f&${attribute}=${value}&plot=short`
+  ).then((data) => data.json());
+}
+
+function createMoviePoster(currMovie) {
+  moviesListElement.insertAdjacentHTML(
+    "beforeend",
+    `<li> 
+    <img src="${currMovie.Poster}" alt="poster of ${currMovie.Title}" >
+    <div>
+      <h2> ${currMovie.Title} </h2> 
+      <p>${currMovie.Ratings[0].Value} </p>
+      <p> ${currMovie.Plot}</p>      
+    </div>
+   </li>`
+  );
+}
+
+function ShowMovies(movieName) {
+  getMovie(movieName)
+  .then((movies) => {
+    const movieList = movies.Search;
+    movieList.forEach((movie) => {
+      getMovie( movie.Title, 't')
+      .then((currMovie) => {
+        createMoviePoster(currMovie);
+     });
+    });
+  });
+} 
+
+ShowMovies('Batman');
+
+searchbar.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const target = event.target;
+  ShowMovies(target.firstElementChild.value)
+  target.firstElementChild.value = "";
+})
