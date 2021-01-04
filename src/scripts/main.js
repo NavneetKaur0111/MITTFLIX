4272e1a7fc916e93fd56a52c7351f3d1fbd1a422
navneet <navneet3265@gmail.com>
2021-01-04 16:36:11 -0600

    add pagination to the page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,19 @@
 const moviesListElement = document.querySelector(".movies");
 const searchbar = document.querySelector("form");
+const nextPage = document.querySelector('#next');
+const prevPage = document.querySelector('#previous');
+let pageNumber = 1;
+let movieName = 'batman';
 
-function getMovie(value  , attribute = 's') {
+function getMovie(value, attribute = 's') {
   return fetch(
     `https://omdbapi.com/?apikey=89e9df8f&${attribute}=${value}&plot=short`
+  ).then((data) => data.json());
+}
+
+function getMovies(value, attribute = 's') {
+  return fetch(
+    `https://omdbapi.com/?apikey=89e9df8f&${attribute}=${value}&plot=short&page=${pageNumber}`
   ).then((data) => data.json());
 }
 
@@ -24,10 +34,28 @@ function createMoviePoster(currMovie) {
   );
 }
 
-function ShowMovies(movieName) {
-  getMovie(movieName)
+const displaynext = (movies) => {
+  const totalPages = movies.totalResults / 10;
+
+  if(pageNumber > 1) {
+    prevPage.classList.remove('hidden');
+  } else {
+    prevPage.classList.add('hidden');
+  }
+
+  if(Math.ceil(totalPages) !== pageNumber) {
+    nextPage.classList.remove('hidden');
+  } else {
+    nextPage.classList.add('hidden');
+  }
+}
+
+function ShowMovies() {
+  moviesListElement.innerHTML = "";
+  getMovies(movieName)
   .then((movies) => {
     const movieList = movies.Search;
+    displaynext(movies)
     movieList.forEach((movie) => {
       getMovie( movie.Title, 't')
       .then((currMovie) => {
@@ -37,12 +65,30 @@ function ShowMovies(movieName) {
   });
 } 
 
-ShowMovies('Batman');
+ShowMovies();
 
 searchbar.addEventListener('submit', (event) => {
   event.preventDefault()
-  moviesListElement.innerHTML = "";
   const target = event.target;
-  ShowMovies(target.firstElementChild.value)
+  movieName = target.firstElementChild.value;
+  pageNumber = 1; 
+  ShowMovies()
   target.firstElementChild.value = "";
+})
+
+prevPage.addEventListener('click', (event) => {
+  pageNumber--;
+  ShowMovies();
+})
+
+nextPage.addEventListener('click', (event)=> {
+  pageNumber++;
+  ShowMovies();
+})
+
+document.querySelector('.page-number').addEventListener('click', (event) => {
+  if(event.target.nodeName === "A") {
+    pageNumber = event.target.dataset.number;
+    ShowMovies();
+  }
 })
